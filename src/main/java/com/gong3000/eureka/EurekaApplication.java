6ae8d0ab49fe3da3811e9925a73bd74bb2c2304b
pgwt <notbad_wangt@126.com>
2018-01-05 16:20:07 +0800

    feat:添加ping接口与exit接口

--- src/main/java/com/gong3000/eureka/EurekaApplication.java
+++ src/main/java/com/gong3000/eureka/EurekaApplication.java
@@ -3,15 +3,29 @@ package com.gong3000.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author tao.wang
  */
 @EnableEurekaServer
+@RestController
 @SpringBootApplication
 public class EurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
+    }
+
+    @RequestMapping({"/ping"})
+    public String ping() {
+        return "success";
+    }
+
+    @RequestMapping({"/exit"})
+    public String exit() {
+        System.exit(0);
+        return "success";
     }
 }
